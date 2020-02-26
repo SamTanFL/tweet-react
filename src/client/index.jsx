191777a4b24ddeaaad82497628c0075f6ce9bea0
SamTanFL <samsite_tan@hotmail.com>
2020-02-26 20:44:08 +0800

    added tweet class

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,17 +4,28 @@ import ReactDOM from 'react-dom';
 import tweets from 'tweets'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        tweets
-      </div>
-    );
-  }
+    render() {
+        const kanyeTweets = tweets.map(tweet => {
+
+        });
+        return (
+            <div>
+                {kanyeTweets}
+            </div>
+        );
+    }
+}
+
+class Tweet extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
 }
 
 const element = document.getElementById('app');
 
 ReactDOM.render(<App />, element );//
-
-console.log("tweet react");
